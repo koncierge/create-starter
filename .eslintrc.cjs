723575841807eca9d7660f13ec46eff9019a4b9f
Matt Pfeffer <matt@koncierge.com.au>
2023-03-17 13:49:52 +1000

    chore(config): improve linting rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,7 @@ module.exports = {
     plugins: ['@typescript-eslint'],
     settings: {},
     rules: {
-        'no-console': 'warn'
+        'no-console': 'warn',
+        'no-protoype-builtins': 'disable'
     }
 };
--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -7,6 +7,20 @@ module.exports = {
             {
                 ignoreAtRules: ['tailwind', 'apply', 'layer', 'variants', 'responsive', 'screen']
             }
-        ]
+        ],
+        'function-no-unknown': [
+            true,
+            {
+                ignoreFunctions: ['theme', 'screen']
+            }
+        ],
+        'value-keyword-case': [
+            'lower',
+            {
+                ignoreFunctions: ['theme']
+            }
+        ],
+        'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global'] }],
+        'custom-property-empty-line-before': ['never']
     }
 };
